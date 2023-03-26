ddbaea645b77ccdedcfa86261526b00620e37b58
Yashvardhan Singh <yashvardhansingh998@gmail.com>
2023-03-25 21:08:48 -0400

    Added pretty printing of websites perf data.

--- benchmarking/website_benchmarking.py
+++ benchmarking/website_benchmarking.py
@@ -1,31 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from collections import defaultdict
+from prettytable import PrettyTable
 
-iterations_per_webpage = 5
+# benchmarking/075-basic-website-performance-testing-backendperformance.png
+# contains the different phases of a request
+# navigationStart -> redirectStart -> redirectEnd -> fetchStart ->
+# domainLookupStart -> domainLookupEnd -> connectStart ->
+# secureConnectionStart -> connectEnd -> requestStart -> responseStart ->
+# responseEnd
+# More details at: https://www.go-euc.com/basic-website-performance-testing/
+# Some observations:
+# 1. redirect period doesn't get triggered
+# 2. the diff b/w fetchStart and navigationStart becomes negligible after
+# first website load
+# 3. The diff b/w domainLookupStart and fetchStart i.e. the App Cache period
+# becomes negligible after the first website load
+# 4. The DNS lookup period becomes 0 after the first load(sometimes 0 even on
+# the first website load)
+# 5. There is negligible time difference b/w domainLookupEnd and connectStart
+# 6. secureConnection seems to be needed to be established once and 0 time
+# required to establish secure connection.
+
+ITERATIONS_PER_WEBPAGE = 5
 website = "http://google.com"
 timings = defaultdict(list)
+WEBSITES_FILE_NAME = "popular_websites.txt"
+HTTP_STR = "http://"
+INDEX_NAME = "Index No."
+BACKEND_PERF_NAME_CONST = "Backend Time(ms)"
+BACKEND_PERF_CONST = "backend"
+FRONTEND_PERF_NAME_CONST = "Frontend Time(ms)"
+FRONTEND_PERF_CONST = "frontend"
+TCP_HANDSHAKE_PERF_NAME_CONST = "TCP Handshake Time(ms)"
+TCP_HANDSHAKE_PERF_CONST = "tcp_handshake"
+RESPONSE_PERF_NAME_CONST = "Server Response Time(ms)"
+RESPONSE_PERF_CONST = "response"
 
 def perfMeasure(driver, website):
-  # benchmarking/075-basic-website-performance-testing-backendperformance.png
-  # contains the different phases of a request
-  # navigationStart -> redirectStart -> redirectEnd -> fetchStart ->
-  # domainLookupStart -> domainLookupEnd -> connectStart ->
-  # secureConnectionStart -> connectEnd -> requestStart -> responseStart ->
-  # responseEnd
-  # More details at: https://www.go-euc.com/basic-website-performance-testing/
-  # Some observations:
-  # 1. redirect period doesn't get triggered
-  # 2. the diff b/w fetchStart and navigationStart becomes negligible after
-  # first website load
-  # 3. The diff b/w domainLookupStart and fetchStart i.e. the App Cache period
-  # becomes negligible after the first website load
-  # 4. The DNS lookup period becomes 0 after the first load(sometimes 0 even on
-  # the first website load)
-  # 5. There is negligible time difference b/w domainLookupEnd and connectStart
-  # 6. secureConnection seems to be needed to be established once and 0 time
-  # required to establish secure connection.
-  # 7. 
   driver.get(website)
   # TODO(Yash) This is deprecated. Update with the new api.
   navigationStart = driver.execute_script("return window.performance.timing.navigationStart")
@@ -48,26 +60,33 @@ def perfMeasure(driver, website):
   frontendPerformance_calc = domComplete - responseStart
   response_diff = responseEnd - requestStart
   
-  print("Back End: %s ms" % backendPerformance_calc)
-  print("Front End: %s ms" % frontendPerformance_calc)
-  print("Tcp Handshake: %s ms" % (connectEnd - connectStart))
-  print("response time: %s ms" % (responseEnd - requestStart))
-  print("Time to fetch: %s ms" % (responseEnd - fetchStart))
+  # print(website)
+  # print("Back End: %s ms" % backendPerformance_calc)
+  # print("Front End: %s ms" % frontendPerformance_calc)
+  # print("Tcp Handshake: %s ms" % (connectEnd - connectStart))
+  # print("response time: %s ms" % (responseEnd - requestStart))
+  perfDict = {}
+  perfDict[BACKEND_PERF_CONST] = backendPerformance_calc
+  perfDict[FRONTEND_PERF_CONST] = frontendPerformance_calc
+  perfDict[TCP_HANDSHAKE_PERF_CONST] = (connectEnd - connectStart)
+  perfDict[RESPONSE_PERF_CONST] = (responseEnd - requestStart)
+  return perfDict
+  # print("Time to fetch: %s ms" % (responseEnd - fetchStart))
   # print("Navigantion Start: %s ms" % navigationStart)
   # print("Redirect Start: %s ms" % redirectStart)
-  timings[0].append(redirectStart - navigationStart)
-  timings[1].append(redirectEnd - redirectStart)
-  timings[2].append(fetchStart -  redirectEnd)
-  timings[3].append(domainLookupStart - fetchStart)
-  timings[4].append(domainLookupEnd - domainLookupStart)
-  timings[5].append(connectStart - domainLookupEnd)
-  timings[6].append(secureConnectionStart - connectStart)
-  timings[7].append(connectEnd - secureConnectionStart)
-  # Time diff in sending a request after establishing connection
-  timings[8].append(requestStart - connectEnd)
-  timings[9].append(responseStart - requestStart)
-  timings[10].append(responseEnd - responseStart)
-  timings[11].append(fetchStart - navigationStart)
+  # timings[0].append(redirectStart - navigationStart)
+  # timings[1].append(redirectEnd - redirectStart)
+  # timings[2].append(fetchStart -  redirectEnd)
+  # timings[3].append(domainLookupStart - fetchStart)
+  # timings[4].append(domainLookupEnd - domainLookupStart)
+  # timings[5].append(connectStart - domainLookupEnd)
+  # timings[6].append(secureConnectionStart - connectStart)
+  # timings[7].append(connectEnd - secureConnectionStart)
+  # # Time diff in sending a request after establishing connection
+  # timings[8].append(requestStart - connectEnd)
+  # timings[9].append(responseStart - requestStart)
+  # timings[10].append(responseEnd - responseStart)
+  # timings[11].append(fetchStart - navigationStart)
   # timings[12].append(fetchStart - unloadEventEnd)
   # print("Different Phases output:")
   # print("B/w navStart redirectStart %s ms" % (redirectStart - navigationStart))
@@ -82,6 +101,9 @@ def perfMeasure(driver, website):
   # print("B/w requestStart responseStart %s ms" % (responseStart - requestStart))
   # print("B/w responseStart responseEnd %s ms" % (responseEnd - responseStart))
 
+def perfMeasureUpdated(driver, website):
+  driver.get(website)
+
 def initialiseWebDriver():
   # chrome_options = webdriver.ChromeOptions()
   # chrome_options.add_argument("--incognito")
@@ -91,10 +113,29 @@ def initialiseWebDriver():
 
 def calculatePerfWebsite(website):
   driver = initialiseWebDriver()
-  for i in range(iterations_per_webpage):
-    perfMeasure(driver, website)
+  print(website)
+  table = PrettyTable()
+  table.field_names = [INDEX_NAME, BACKEND_PERF_NAME_CONST,
+                       FRONTEND_PERF_NAME_CONST, TCP_HANDSHAKE_PERF_NAME_CONST,
+                       RESPONSE_PERF_NAME_CONST]
+  # out_str = ""
+  for i in range(ITERATIONS_PER_WEBPAGE):
+    perfDict = perfMeasure(driver, website)
+    table.add_row([i, perfDict[BACKEND_PERF_CONST],
+                   perfDict[FRONTEND_PERF_CONST],
+                   perfDict[TCP_HANDSHAKE_PERF_CONST],
+                   perfDict[RESPONSE_PERF_CONST]])
+    # out_str = str(i) + " "
+    # out_str += perfOutput(perfDict)
+    # print(out_str)
   driver.quit()
-  printTimingsDebug()
+  print(table)
+  # printTimingsDebug()
+
+def perfOutput(perfDict):
+  out_str = ""
+  # 
+  return out_str
 
 def printTimingsDebug():
   print("Diffs")
@@ -104,5 +145,23 @@ def printTimingsDebug():
       st = st + str(j) + " "
     print(st)
 
-calculatePerfWebsite(website)
+def readPopularWebsites():
+  with open(WEBSITES_FILE_NAME, 'r') as f:
+    websites_list = f.readlines()
+  f.close()
+  return websites_list
 
+def sanitizeWebsitesList(websites_list):
+  websites_list_sanitized = []
+  for website in websites_list:
+    websites_list_sanitized.append(HTTP_STR + website)
+  return websites_list_sanitized
+
+def main():
+  websites_list = readPopularWebsites()
+  websites_list = sanitizeWebsitesList(websites_list)
+  for website in websites_list:
+    calculatePerfWebsite(website)
+
+if __name__ == "__main__":
+  main()
