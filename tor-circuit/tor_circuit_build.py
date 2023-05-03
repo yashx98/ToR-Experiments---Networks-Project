c8eb049d92d32477c4fee7fcddbce63ebcecd48f
Yashvardhan Singh <yashvardhansingh998@gmail.com>
2023-05-03 14:12:10 -0400

    Added changes for manually choosing countries

--- tor-circuit/tor_circuit_build.py
+++ tor-circuit/tor_circuit_build.py
@@ -24,6 +24,7 @@ NUM_ITER_FOR_COLLECTION = 30
 NUM_CIRCUITS_SETUP = 100
 COUNTRIES_TORRC_LINE_NUM = 256
 NUM_TRIES_COUNTRY_CIRCUIT = 5
+NUM_CIRCUITS_PARALLEL = 10
 FINGERPRINT_STR = "Fingerprint"
 IP_ADDRESS_STR = "IP Address"
 LOCALE_STR = "Locale"
@@ -34,6 +35,7 @@ MIDDLE_NODE_STR = "Middle Node"
 MEAN_STR = "Mean"
 MEADIAN_STR = "Median"
 STD_DEV_STR = "Standard Deviation"
+SUCCESS_STR = "Success Rate"
 COF_VAR_STR = "Coefficient of Variation"
 NORTH_AMERICA_STR = "North America"
 EUROPE_STR = "Europe"
@@ -50,6 +52,11 @@ def remakeTorWithChangedHops(num_hops):
   tor_process = subprocess.Popen("exec tor --controlport 9201", cwd="/users/ys5608/code/tor/", shell=True)
   time.sleep(30)
   return tor_process
+
+def renewConnection():
+  with Controller.from_port(port = CONTROL_PORT) as controller:
+    controller.authenticate(password="")
+    controller.signal(Signal.NEWNYM)
 
 def circuitBuildTime():
   with Controller.from_port(port = CONTROL_PORT) as controller:
@@ -105,6 +112,7 @@ def calCircuitSetupTimes():
   stats_dict[MEADIAN_STR] = median_time
   stats_dict[STD_DEV_STR] = std_dev_time
   stats_dict[COF_VAR_STR] = coef_var
+  stats_dict[SUCCESS_STR] = (NUM_CIRCUITS_SETUP / num_tries) * 100
   return stats_dict
 
 def listCircuits():
@@ -191,11 +199,11 @@ def measureCircuitBuildTimes():
   print(build_time_stats)
   stats_arr = [MEAN_STR, MEADIAN_STR]
   fig, ax = plt.subplots()
-  x_axis = [0]
+  x_axis = []
   for i in range(2, MAX_NUM_HOPS + 1):
     x_axis.append(i)
   for stat in stats_arr:
-    y_axis = [0]
+    y_axis = []
     for i in range(MAX_NUM_HOPS - 1):
       y_axis.append(build_time_stats[i][stat])
     ax.plot(x_axis, y_axis, marker='o', markersize=4, label=stat + ' Build Time')
@@ -204,6 +212,16 @@ def measureCircuitBuildTimes():
   ax.set_title('Circuit Build Times')
   ax.legend()
   plt.savefig('CircuitBuildTimes.png')
+  fig2, ax2 = plt.subplots()
+  y_axis = []
+  for i in range(MAX_NUM_HOPS - 1):
+    y_axis.append(build_time_stats[i][SUCCESS_STR])
+  ax2.plot(x_axis, y_axis, marker='o', markersize=4, label=stat + ' Build Time')
+  ax2.set_xlabel('Circuit Length')
+  ax2.set_ylabel('Success (%)')
+  ax2.set_title('Circuit Build Build Success Rates %')
+  ax2.legend()
+  plt.savefig('CircuitBuildSuccessRate.png')
   return build_time_stats
 
 def specifyCountryTorrcFile(node_type, countries_list, line_num):
@@ -286,24 +304,18 @@ def runForCountries(countries_list, num_hops):
   clearLinesTorrcFile(COUNTRIES_TORRC_LINE_NUM + 2)
   print("Last circuits list:")
   listCircuits()
-  controller.reset_conf('__DisablePredictedCircuits', '1')
-  controller.reset_conf('MaxOnionsPending', '0')
-  controller.reset_conf('newcircuitperiod', '999999999')
-  controller.reset_conf('maxcircuitdirtiness', '999999999')
+  controller.reset_conf('__DisablePredictedCircuits')
+  controller.reset_conf('MaxOnionsPending')
+  controller.reset_conf('newcircuitperiod')
+  controller.reset_conf('maxcircuitdirtiness')
   print("Set configs reset")
   listCircuits()
+  clearAllCircuits()
   tor_process.send_signal(signal.SIGINT)
   return stats
 
-
-def main():
-  # countries_list = ['us', 'ca']
+def runOverDifferentRegions(countries_list):
   countriesRegions = [NORTH_AMERICA_STR, EUROPE_STR, EAST_ASIA_STR]
-  countries_list = {}
-  countries_list[NORTH_AMERICA_STR] = ["us", "ca"]
-  countries_list[EUROPE_STR] = ["uk", "fr", "de", "dk", "se"]
-  countries_list[EAST_ASIA_STR] = ["jp", "tw", "kr"]
-  # runForCountries(countries_list)
   fig, ax = plt.subplots()
   x_axis = []
   for i in range(2, MAX_NUM_HOPS + 1):
@@ -325,7 +337,182 @@ def main():
   # ax.set_title('Circuit Throughputs in Different Regions')
   # ax.legend()
   # plt.savefig('Throughput.png')
+
+def getListOfNodes(countries_list):
+  controller = stem.control.Controller.from_port(port = CONTROL_PORT)
+  controller.authenticate()
+  nodes = controller.get_network_statuses()
+  # entry_nodes = [node for node in nodes if 'Guard' in node.flags and 'Stable' in node.flags]
+  entry_nodes = []
+  middle_nodes = []
+  exit_nodes = []
+  for node in nodes:
+    try:
+      if 'Guard' in node.flags and 'Stable' in node.flags:
+        locale = controller.get_info("ip-to-country/" + node.address)
+        if locale in countries_list:
+          node_details = {}
+          node_details[FINGERPRINT_STR] = node.fingerprint
+          node_details[IP_ADDRESS_STR] = node.address
+          node_details[LOCALE_STR] = locale
+          node_details[NICKNAME_STR] = node.nickname
+          entry_nodes.append(node_details)
+    except Exception as e:
+      continue
+  for desc in stem.descriptor.remote.get_server_descriptors():
+    if desc.exit_policy.is_exiting_allowed() and desc.exit_policy.is_exiting_allowed():
+      locale = controller.get_info("ip-to-country/" + desc.address)
+      if locale in countries_list:
+        node_details = {}
+        node_details[FINGERPRINT_STR] = desc.fingerprint
+        node_details[IP_ADDRESS_STR] = desc.address
+        node_details[LOCALE_STR] = locale
+        node_details[NICKNAME_STR] = desc.nickname
+        exit_nodes.append(node_details)
+    else:
+      locale = controller.get_info("ip-to-country/" + desc.address)
+      if locale in countries_list:
+        node_details = {}
+        node_details[FINGERPRINT_STR] = desc.fingerprint
+        node_details[IP_ADDRESS_STR] = desc.address
+        node_details[LOCALE_STR] = locale
+        node_details[NICKNAME_STR] = desc.nickname
+        middle_nodes.append(node_details)
+  return entry_nodes, middle_nodes, exit_nodes
+  # exit_nodes = controller.get_info("ns/all-exit-ports")
+  # relays = controller.get_network_statuses()
+  # exits = [relay for relay in relays if relay.exit_policy.is_exiting_allowed()]
+  # exit_ips = [exit.nickname for exit in exits]
+  # for exit_ip in exits:
+  #   locale = controller.get_info("ip-to-country/" + exit_ip.address)
+  #   print(locale + " " + exit_ip.nickname + " " + exit_ip.address)
   # 
+  # 
+  # consensus = stem.descriptor.remote.get_consensus()
+  # for node in consensus.routers.values():
+  #   if node.flags and Flag.GUARD:
+  #     entry_nodes.append(node.address)
+  # downloader = DescriptorDownloader(controller)
+  # relays = downloader.get_server_descriptors()
+  # for relay in relays:
+  #   if Flag.GUARD in relay.flags and relay.address.endswith('.onion'):
+  #     entry_nodes.append(relay.address)
+  # print(entry_nodes)
+
+def returnRandomNode(nodes, num):
+  if len(nodes) == 0:
+    entry, middle, exit = getListOfNodes(["us", "ca", "uk", "fr", "de", "dk"])
+    if num == 0:
+      return random.choice(entry)[NICKNAME_STR]
+    elif num == 1:
+      return random.choice(middle)[NICKNAME_STR]
+    elif num == 2:
+      return random.choice(exit)[NICKNAME_STR]
+  return random.choice(nodes)[NICKNAME_STR]
+
+def setupTorForRun(num_hops):
+  tor_process = remakeTorWithChangedHops(num_hops)
+  controller = stem.control.Controller.from_port(port = CONTROL_PORT)
+  controller.authenticate()
+  controller.set_conf('__DisablePredictedCircuits', '1')
+  controller.set_conf('MaxOnionsPending', '0')
+  controller.set_conf('newcircuitperiod', '999999999')
+  controller.set_conf('maxcircuitdirtiness', '999999999')
+  return tor_process
+
+def cleanupTorAfterRun(tor_process):
+  controller = stem.control.Controller.from_port(port = CONTROL_PORT)
+  controller.authenticate()
+  print("Last circuits list:")
+  listCircuits()
+  controller.reset_conf('__DisablePredictedCircuits')
+  controller.reset_conf('MaxOnionsPending')
+  controller.reset_conf('newcircuitperiod')
+  controller.reset_conf('maxcircuitdirtiness')
+  print("Set configs reset")
+  listCircuits()
+  clearAllCircuits()
+  tor_process.send_signal(signal.SIGINT)
+
+def torCreateCircuits(countries_list):
+  entry, middle, exit = getListOfNodes(countries_list)
+  controller = stem.control.Controller.from_port(port = CONTROL_PORT)
+  controller.authenticate()
+  clearAllCircuits()
+  # Create new circuits randomly
+  index = 0
+  # for i in range(NUM_CIRCUITS_PARALLEL):
+  while index < NUM_CIRCUITS_PARALLEL:
+    # Entry node
+    entry_node = returnRandomNode(entry, 0)
+    # Exit node
+    exit_node = returnRandomNode(exit, 2)
+    seq_nodes = []
+    seq_nodes.append(entry_node)
+    # Middle nodes
+    for i in range(2, MAX_NUM_HOPS):
+      middle_node = returnRandomNode(middle, 1)
+      seq_nodes.append(middle_node)
+    seq_nodes.append(exit_node)
+    try:
+      controller.new_circuit(seq_nodes)
+    except Exception as e:
+      print("Error in creating tor circuit: " + str(e))
+      continue
+    index += 1
+
+def runOverDifferentRegionsManually(countries_list):
+  countriesRegions = []
+  for region in countries_list:
+    countriesRegions.append(region)
+  fig, ax = plt.subplots()
+  x_axis = []
+  for i in range(2, MAX_NUM_HOPS + 1):
+    x_axis.append(i)
+  y_axis = {}
+  for region in countriesRegions:
+    y_axis[region] = []
+  for i in range(2, MAX_NUM_HOPS + 1):
+    tor_process = setupTorForRun(i)
+    for region in countriesRegions:
+      torCreateCircuits(countries_list[region])
+      stats = measureLatency(True)
+      y_axis[region].append(stats[MEAN_STR])
+    cleanupTorAfterRun(tor_process)
+  for region in countriesRegions:
+    ax.plot(x_axis, y_axis[region], marker='o', markersize=4, label=region)
+  ax.set_xlabel('Circuit Length')
+  # 
+  ax.set_ylabel('Time (ms)')
+  ax.set_title('Circuit Latencies in Different Regions')
+  ax.legend()
+  plt.savefig('LatenciesRegionTime.png')
+  # 
+  # ax.set_ylabel('Throughput (MBps)')
+  # ax.set_title('Circuit Throughputs in Different Regions')
+  # ax.legend()
+  # plt.savefig('Throughput.png')
+
+def main():
+  countries_list = {}
+  countries_list[NORTH_AMERICA_STR] = ["us", "ca"]
+  countries_list[EUROPE_STR] = ["uk", "fr", "de", "dk", "se"]
+  countries_list[EAST_ASIA_STR] = ["jp", "tw", "kr"]
+  # runOverDifferentRegions(countries_list)
+  # e, m, x = getListOfNodes(["us", "ca"])
+  # print(e)
+  runOverDifferentRegionsManually(countries_list)
+
+  # listCircuits()
+  # entry, middle, exit = getListOfNodes(countries_list[EAST_ASIA_STR])
+  # createCircuitManually()
+  # print(len(entry))
+
+
+  # 
+  # measureCircuitBuildTimes()
+  # renewConnection()
+  # clearAllCircuits()
   # tor_process = remakeTorWithChangedHops(3)
   # nodes_collected = manualNodeCollection()
   # Measure circuit build times
